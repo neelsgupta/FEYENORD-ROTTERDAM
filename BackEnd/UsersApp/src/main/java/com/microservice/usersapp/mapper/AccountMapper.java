ff57d4d93093b1fdd90d29cc946653339aafc6fd
NEELE-PC\neelesh <neeleshgupta077@gmail.com>
2020-02-20 17:50:50 +0100

    code refactor

--- BackEnd/UsersApp/src/main/java/com/microservice/usersapp/mapper/AccountMapper.java
+++ BackEnd/UsersApp/src/main/java/com/microservice/usersapp/mapper/AccountMapper.java
@@ -1,8 +1,11 @@
 package com.microservice.usersapp.mapper;
 
+import org.springframework.stereotype.Service;
+
 import com.microservice.usersapp.entity.Account;
 import com.microservice.usersapp.entity.ChildAccount;
 
+@Service
 public class AccountMapper {
 	
 	
