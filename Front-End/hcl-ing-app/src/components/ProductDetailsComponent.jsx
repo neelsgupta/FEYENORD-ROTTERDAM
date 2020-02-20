f544e29c4665770befd9b6f609c1cbf0900459db
izharuddin khan <izharuddin.khan@digib.com>
2020-02-20 16:47:07 +0100

    changes

--- Front-End/hcl-ing-app/src/components/ProductDetailsComponent.jsx
+++ Front-End/hcl-ing-app/src/components/ProductDetailsComponent.jsx
@@ -1,29 +1,25 @@
 
 import React from 'react';
-import { CSSObject } from 'styled-components';
+import styled from "styled-components";
 
-const formStyle: CSSObject = {
-    background: '#ffffff',
-    border: '1px solid rgba(0,0,0,0.1)',
-    borderRadius: 3,
-    boxShadow: '0 1px 2px rgba(0,0,0,.05)',
-    width: '50%',
-    margin: 20,
-    padding: 20,
-};
+const Card = styled.div`
+  height: auto;
+  width: auto;
+  text-align: left;
+`;
 
 export function ProductDetailsBox() {
     return (
-        <div style={formStyle}>
+        <Card >
             <h1>Produktdetails</h1>
-            <h3>Selected product group:</h3>
+            <hr></hr>
+            <h3>Selected product group</h3>
+            <hr></hr>
 
             <>
                 <h3>Aaaaa:</h3>
-
             </>
-
             <hr></hr>
-        </div>
+        </Card>
     )
 }
